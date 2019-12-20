2d37d3a2a56edb40fd8ec9d3253e284e49a3824a
pallavigujare <59077193+pallavigujare@users.noreply.github.com>
2019-12-20 10:36:01 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+time.sleep(3)
+pyautogui.click((pyautogui.locateCenterOnScreen('One.PNG')))
+time.sleep(2)
+pyautogui.click((pyautogui.locateCenterOnScreen('One.PNG')))
+time.sleep(2)
+pyautogui.click((pyautogui.locateCenterOnScreen('plus.PNG')))
+time.sleep(2)
+pyautogui.click((pyautogui.locateCenterOnScreen('One.PNG')))
+time.sleep(2)
+pyautogui.click((pyautogui.locateCenterOnScreen('One.PNG')))
+time.sleep(2)
+pyautogui.press('enter')
